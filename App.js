1a889eea760b440bfe920c07687512bf7544b569
Marwan Atef <marwanatef97@gmail.com>
2020-04-20 13:21:16 +0200

    initial commit

--- App.js
+++ App.js
@@ -4,7 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>
+        {"             "}Tito{"\n"}
+        احنا دبابات عالكباسات
+      </Text>
     </View>
   );
 }
