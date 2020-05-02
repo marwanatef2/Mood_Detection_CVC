b4d0a56bd3b2253d3ba9cd7c45f7844d4ea8d36b
Marwan Atef <marwanatef97@gmail.com>
2020-05-02 05:35:44 +0200

    added friends models and routes

--- api/models.py
+++ api/models.py
@@ -5,11 +5,40 @@ from flask_dance.consumer.storage.sqla import OAuthConsumerMixin
 
 db = SQLAlchemy()
 
+followers = db.Table('followers',
+                     db.Column('follower_id', db.Integer, db.ForeignKey(
+                         'user.id'), primary_key=True),
+                     db.Column('followed_id', db.Integer,
+                               db.ForeignKey('user.id'), primary_key=True)
+                     )
+
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    # name = db.Column(db.String(80), nullable=False)
+    name = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    image = db.Column(db.String(200), unique=True)
+    score = db.Column(db.Integer)
+    following = db.relationship('User', secondary=followers,
+                                primaryjoin=(followers.c.follower_id == id),
+                                secondaryjoin=(followers.c.followed_id == id),
+                                backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+
+    def __repr__(self):
+        return '<User {}>'.format(self.email)
+
+    def is_following(self, user):
+        return self.following.filter(
+            followers.c.followed_id == user.id
+        ).count() == 1
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.following.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.following.remove(user)
 
 
 class OAuth(OAuthConsumerMixin, db.Model):
--- api/server.py
+++ api/server.py
@@ -1,6 +1,6 @@
 from flask import Flask, redirect, url_for, render_template
 from flask_login import login_required, logout_user, current_user
-from models import db, login_manager
+from models import db, login_manager, User
 from oauth import blueprint
 # from flask_dance.contrib.facebook import make_facebook_blueprint, facebook
 from flask_dance.contrib.google import make_google_blueprint, google
@@ -19,17 +19,9 @@ def home():
     if current_user.is_authenticated:
         resp = google.get('/oauth2/v1/userinfo')
         if resp.ok:
-            return render_template('zeez.html', userinfo = resp.json())
+            return render_template('zeez.html', userinfo=resp.json())
     else:
         return "hello nobody"
-
-
-@app.route('/about')
-@login_required
-def about():
-    # resp = facebook.get('/me')
-    resp = google.get('/oauth2/v1/userinfo')
-    return resp.json() if resp.ok else False
 
 
 @app.route('/login')
@@ -38,14 +30,75 @@ def login():
     return redirect(url_for("google.login"))
 
 
-@app.route('/zeez')
-def zeez():
-    return render_template('zeez.html')
-
-
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
-    # return "logged out"
     return redirect(url_for('home'))
+
+
+# @login_required
+@app.route('/<myemail>/follow/<email>')
+def follow_user(myemail, email):
+    me = User.query.filter_by(email=myemail).first()
+    user = User.query.filter_by(email=email).first()
+    # if user is not None and user != current_user:
+    if user is not None and user != me:
+        # current_user.follow(user)
+        me.follow(user)
+        db.session.commit()
+        return {'followed': True}
+    return {'followed': False}
+
+
+# @login_required
+@app.route('/<myemail>/unfollow/<email>')
+def unfollow_user(myemail, email):
+    me = User.query.filter_by(email=myemail).first()
+    user = User.query.filter_by(email=email).first()
+    # if user is not None and user != current_user:
+    if user is not None and user != me:
+        # current_user.unfollow(user)
+        me.unfollow(user)
+        db.session.commit()
+        return {'unfollowed': True}
+    return {'unfollowed': False}
+
+
+# @login_required
+@app.route('/<myemail>/friends')
+@app.route('/<myemail>/following')
+def followed_users(myemail):
+    me = User.query.filter_by(email=myemail).first()
+    # following = current_user.following
+    following = me.following
+    users = []
+    for followed in following:
+        users.append({'name': followed.name, 'email': followed.email})
+    return {'users': users}
+
+
+@app.route('/<myemail>/followers')
+def followers(myemail):
+    me = User.query.filter_by(email=myemail).first()
+    # following = current_user.following
+    following = me.followers
+    users = []
+    for followed in following:
+        users.append({'name': followed.name, 'email': followed.email})
+    return {'users': users}
+
+
+@app.route('/db')
+def database():
+    # db.drop_all()
+    # db.create_all()
+    marwan = User.query.filter_by(name='Marwan').first()
+    # zeez = User.query.filter_by(name='Zeez').first()
+    # samir = User.query.filter_by(name='Samir').first()
+    salma = User.query.filter_by(name='Salma').first()
+    salma.follow(marwan)
+    db.session.commit()
+    users = marwan.followers
+    names = [user.name for user in users]
+    return {'users': names}
